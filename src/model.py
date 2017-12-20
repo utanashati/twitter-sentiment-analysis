665ec33b4116194328fc6807427559157ee409bb
utanashati <utanashati@mail.ru>
2017-12-20 15:16:16 +0100

    Set proper indentation

--- src/model.py
+++ src/model.py
@@ -12,26 +12,26 @@ RECEPTIVE_FIELDS = [1, 2, 3, 4, 5, 6]
 OUTPUT_FILTERS = [25, 50, 75, 100, 125, 150]
 
 def compile_CharLSTM():
-	model = Sequential()
+    model = Sequential()
 
-	model.add(Conv2D(OUTPUT_FILTERS[0], (1, RECEPTIVE_FIELDS[0]), activation='relu', \
-	                 input_shape=(MAX_WORDS_TWEET, MAX_CHARS_WORD, len(ALPHABET))))
-	print('Conv2D 1: ' + str(model.output_shape))
-	for output_filters, receptive_field in zip(OUTPUT_FILTERS[1:], RECEPTIVE_FIELDS[1:]):
-	    model.add(Conv2D(output_filters, (1, receptive_field), activation='relu'))
-	    print('Conv2D {}: '.format(receptive_field) + str(model.output_shape))
+    model.add(Conv2D(OUTPUT_FILTERS[0], (1, RECEPTIVE_FIELDS[0]), activation='relu', \
+                     input_shape=(MAX_WORDS_TWEET, MAX_CHARS_WORD, len(ALPHABET))))
+    print('Conv2D 1: ' + str(model.output_shape))
+    for output_filters, receptive_field in zip(OUTPUT_FILTERS[1:], RECEPTIVE_FIELDS[1:]):
+        model.add(Conv2D(output_filters, (1, receptive_field), activation='relu'))
+        print('Conv2D {}: '.format(receptive_field) + str(model.output_shape))
 
-	model.add(MaxPooling2D((1, 2), data_format='channels_first'))
-	print('MaxPooling2D: ' + str(model.output_shape))
-	model.add(Reshape((model.output_shape[1], model.output_shape[3])))
-	print('Reshape: ' + str(model.output_shape))
+    model.add(MaxPooling2D((1, 2), data_format='channels_first'))
+    print('MaxPooling2D: ' + str(model.output_shape))
+    model.add(Reshape((model.output_shape[1], model.output_shape[3])))
+    print('Reshape: ' + str(model.output_shape))
 
-	model.add(LSTM(units=650))
-	print('LSTM: ' + str(model.output_shape))
+    model.add(LSTM(units=650))
+    print('LSTM: ' + str(model.output_shape))
 
-	model.add(Dense(1, activation='sigmoid'))
-	print('Dense: ' + str(model.output_shape))
+    model.add(Dense(1, activation='sigmoid'))
+    print('Dense: ' + str(model.output_shape))
 
-	model.compile(optimizer='adam', loss='binary_crossentropy')
+    model.compile(optimizer='adam', loss='binary_crossentropy')
 
-	return model
+    return model
--- src/utils.py
+++ src/utils.py
@@ -30,27 +30,27 @@ MAX_CHARS_WORD = 16
 BATCHSIZE = 32
 
 def get_train_steps(paths=PATHS_TRAIN, batchsize=BATCHSIZE):
-	counter = 0
-	for path in paths:
-		with open(path, 'r', encoding='latin-1') as f:
-			for line in f:
-				counter += 1
+    counter = 0
+    for path in paths:
+        with open(path, 'r', encoding='latin-1') as f:
+            for line in f:
+                counter += 1
 
-	return counter // batchsize
+    return counter // batchsize
 
 def get_valid_steps(path=PATH_VALID_FULL, batchsize=BATCHSIZE):
-	counter = 0
-	with open(path, 'r', encoding='latin-1') as f:
-		for line in f:
-			counter += 1
+    counter = 0
+    with open(path, 'r', encoding='latin-1') as f:
+        for line in f:
+            counter += 1
 
-	return counter // batchsize
+    return counter // batchsize
 
 def txt_to_tweets_train(path):
     tweets = []
     with open(path, 'r', encoding='latin-1') as f:
         for tweet in f:
-        	tweets.append(tweet[:-1]) # don't read last '\n'
+            tweets.append(tweet[:-1]) # don't read last '\n'
             
     return tweets
 
@@ -64,132 +64,130 @@ def txt_to_tweets_test(path=PATH_TEST):
     return tweets
 
 def print_all(np_array):
-	np.set_printoptions(threshold=np.nan)
-	print(np_array)
-	np.set_printoptions()
+    np.set_printoptions(threshold=np.nan)
+    print(np_array)
+    np.set_printoptions()
 
 def cleanup(tweets, path=None):
-	tweets = list(map(lambda t: t.replace(u'\u201c', '"').replace(u'\u201d', '"'), tweets)) # remove curly quotes
-	tweets = list(map(lambda t: t.replace('<user> ', ''), tweets)) # remove <user>
-	tweets = list(map(lambda t: t.replace('<url> ', '').strip(), tweets)) # remove <url>
-	tweets = list(set(tweets)) # remove duplicates
-	tweets = list(filter(None, tweets)) # delete empty tweets
-	if path:
-	    with open(path, 'w') as f:
-	        for tweet in tweets:
-	            f.write(tweet + '\n')
+    tweets = list(map(lambda t: t.replace(u'\u201c', '"').replace(u'\u201d', '"'), tweets)) # remove curly quotes
+    tweets = list(map(lambda t: t.replace('<user> ', ''), tweets)) # remove <user>
+    tweets = list(map(lambda t: t.replace('<url> ', '').strip(), tweets)) # remove <url>
+    tweets = list(set(tweets)) # remove duplicates
+    tweets = list(filter(None, tweets)) # delete empty tweets
+    if path:
+        with open(path, 'w') as f:
+            for tweet in tweets:
+                f.write(tweet + '\n')
 
-	return tweets	           
+    return tweets              
 
 def create_training_set(tweets_pos, tweets_neg, valid_part=0.05, path_train=PATH_TRAIN_FULL, path_valid=PATH_VALID_FULL):
-	concat = lambda x, y: np.concatenate((np.ones(len(x), dtype=int)[:, np.newaxis]*y, np.array(x)[:, np.newaxis]), axis=1)
-	train = np.concatenate((concat(tweets_pos, 1), concat(tweets_neg, 0)), axis=0)
-	np.random.seed(42)
-	np.random.shuffle(train)
-	cut_point = int(len(train)*(1-valid_part)) - 1
-	np.savetxt(path_train, train[:cut_point], fmt='%s', delimiter='--')
-	np.savetxt(path_valid, train[cut_point:, :], fmt='%s', delimiter='--')
+    concat = lambda x, y: np.concatenate((np.ones(len(x), dtype=int)[:, np.newaxis]*y, np.array(x)[:, np.newaxis]), axis=1)
+    train = np.concatenate((concat(tweets_pos, 1), concat(tweets_neg, 0)), axis=0)
+    np.random.seed(42)
+    np.random.shuffle(train)
+    cut_point = int(len(train)*(1-valid_part)) - 1
+    np.savetxt(path_train, train[:cut_point], fmt='%s', delimiter='--')
+    np.savetxt(path_valid, train[cut_point:, :], fmt='%s', delimiter='--')
 
 def tokenize(tweets):
-	for t, tweet in enumerate(tweets):
-		tweets[t] = tweet.split(' ')
+    for t, tweet in enumerate(tweets):
+        tweets[t] = tweet.split(' ')
 
-	return tweets
+    return tweets
 
 def get_max_lengths(tweets_tokd):
-	max_words_tweet = 0
-	max_chars_word = 0
-	for tweet in tweets_tokd:
-		if len(tweet) > max_words_tweet:
-			max_words_tweet = len(tweet)
-		for word in tweet:
-			if len(word) > max_chars_word:
-				max_chars_word = len(word)
+    max_words_tweet = 0
+    max_chars_word = 0
+    for tweet in tweets_tokd:
+        if len(tweet) > max_words_tweet:
+            max_words_tweet = len(tweet)
+        for word in tweet:
+            if len(word) > max_chars_word:
+                max_chars_word = len(word)
 
-	return max_words_tweet, max_chars_word
+    return max_words_tweet, max_chars_word
 
 def get_lengths_distrs(tweets_tokd, max_lengths):
-	max_words_tweet, max_chars_word = max_lengths
-	words_tweet = [0] * max_words_tweet
-	chars_word = [0] * max_chars_word
-	for tweet in tweets_tokd:
-		words_tweet[len(tweet)-1] += 1
-		for word in tweet:
-			chars_word[len(word)-1] += 1
+    max_words_tweet, max_chars_word = max_lengths
+    words_tweet = [0] * max_words_tweet
+    chars_word = [0] * max_chars_word
+    for tweet in tweets_tokd:
+        words_tweet[len(tweet)-1] += 1
+        for word in tweet:
+            chars_word[len(word)-1] += 1
 
-	return words_tweet, chars_word
+    return words_tweet, chars_word
 
 def cut_lengths(tweets_tokd, max_words_tweet=MAX_WORDS_TWEET, max_chars_word=MAX_CHARS_WORD):
-	for t, tweet in enumerate(tweets_tokd):
-		words_to_delete = []
-		for w, word in enumerate(tweet):
-			if len(word) > MAX_CHARS_WORD:
-				words_to_delete.append(w)
-		tmp = tweets_tokd[t]
-		del tweets_tokd[t]
-		tweets_tokd.insert(t, list(np.delete(tmp, words_to_delete, None)))
+    for t, tweet in enumerate(tweets_tokd):
+        words_to_delete = []
+        for w, word in enumerate(tweet):
+            if len(word) > MAX_CHARS_WORD:
+                words_to_delete.append(w)
+        tmp = tweets_tokd[t]
+        del tweets_tokd[t]
+        tweets_tokd.insert(t, list(np.delete(tmp, words_to_delete, None)))
 
-	for t, tweet in enumerate(tweets_tokd):
-		if len(tweet) > MAX_WORDS_TWEET:
-			tweets_tokd[t] = tweet[:MAX_WORDS_TWEET]
+    for t, tweet in enumerate(tweets_tokd):
+        if len(tweet) > MAX_WORDS_TWEET:
+            tweets_tokd[t] = tweet[:MAX_WORDS_TWEET]
 
-	return tweets_tokd
+    return tweets_tokd
 
 def tweet_to_onehot(tweet_tokd):
-	onehot = np.zeros((MAX_WORDS_TWEET, MAX_CHARS_WORD, len(ALPHABET)))
-	for w, word in enumerate(tweet_tokd):
-		for c, char in enumerate(word):
-			if char not in DICT_CHAR_TO_IND:
-				continue
-			else:
-				onehot[w, c, DICT_CHAR_TO_IND[char]] = 1
+    onehot = np.zeros((MAX_WORDS_TWEET, MAX_CHARS_WORD, len(ALPHABET)))
+    for w, word in enumerate(tweet_tokd):
+        for c, char in enumerate(word):
+            if char not in DICT_CHAR_TO_IND:
+                continue
+            else:
+                onehot[w, c, DICT_CHAR_TO_IND[char]] = 1
 
-	return onehot
+    return onehot
 
 def tweets_to_batch(tweets_tokd_batch, batchsize=BATCHSIZE):
-	assert len(tweets_tokd_batch) == batchsize
-	batch = np.zeros((batchsize, MAX_WORDS_TWEET, MAX_CHARS_WORD, len(ALPHABET)))
-	for i in range(batchsize):
-		batch[i] = tweet_to_onehot(tweets_tokd_batch[i])
+    assert len(tweets_tokd_batch) == batchsize
+    batch = np.zeros((batchsize, MAX_WORDS_TWEET, MAX_CHARS_WORD, len(ALPHABET)))
+    for i in range(batchsize):
+        batch[i] = tweet_to_onehot(tweets_tokd_batch[i])
 
-	return batch
+    return batch
 
 def split_txt(path, n_chunks):
-	counter = 0
-	with open(path, 'r', encoding='latin-1') as f:
-		for line in f:
-			counter += 1
+    counter = 0
+    with open(path, 'r', encoding='latin-1') as f:
+        for line in f:
+            counter += 1
 
-	with open(path, 'r', encoding='latin-1') as f:
-		chunk_size = counter // n_chunks
-		for i in range(n_chunks):
-			if i == n_chunks - 1:
-				remaining = -1
-			else:
-				remaining = chunk_size
-			with open(path[:-4] + '_{}.txt'.format(i+1), 'w', encoding='latin-1') as chunk:
-				for line in f:
-					chunk.write(line)
-					remaining -= 1
-					if remaining == 0:
-						break
+    with open(path, 'r', encoding='latin-1') as f:
+        chunk_size = counter // n_chunks
+        for i in range(n_chunks):
+            if i == n_chunks - 1:
+                remaining = -1
+            else:
+                remaining = chunk_size
+            with open(path[:-4] + '_{}.txt'.format(i+1), 'w', encoding='latin-1') as chunk:
+                for line in f:
+                    chunk.write(line)
+                    remaining -= 1
+                    if remaining == 0:
+                        break
 
 def iterate_batches(paths=PATHS_TRAIN, batchsize=BATCHSIZE):
-	inputs, targets = [], []
-	remaining = batchsize
-	for path in paths:
-		with open(path, 'r', encoding='latin-1') as t:
-			for tweet in t:
-				target, input_ = tweet.split('--', 1)
-				input_ = input_[:-1]
-				inputs.append(input_)
-				targets.append(int(target))
-				remaining -= 1
-				if remaining == 0:
-					inputs = cut_lengths(tokenize(inputs))
-					inputs = tweets_to_batch(inputs, batchsize)
-					yield inputs, np.array(targets)
-					inputs, targets = [], []
-					remaining = batchsize
-	
-	
+    inputs, targets = [], []
+    remaining = batchsize
+    for path in paths:
+        with open(path, 'r', encoding='latin-1') as t:
+            for tweet in t:
+                target, input_ = tweet.split('--', 1)
+                input_ = input_[:-1]
+                inputs.append(input_)
+                targets.append(int(target))
+                remaining -= 1
+                if remaining == 0:
+                    inputs = cut_lengths(tokenize(inputs))
+                    inputs = tweets_to_batch(inputs, batchsize)
+                    yield inputs, np.array(targets)
+                    inputs, targets = [], []
+                    remaining = batchsize
